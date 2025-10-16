039364912c5d309c66c9c50b44ccf821a6496cc6
Ryan de Morais Correia <ryan.morais.workspace@gmail.com>
2025-10-16 12:24:29 -0300

    DELETADO

--- primeira-atividade/function-sum/main.js
+++
@@ -1,6 +0,0 @@
-import { somar } from "./soma.js";
-
-console.log(somar(1,2));
-console.log(somar(-10, 20));
-console.log(somar(5, 10));
-
--- primeira-atividade/function-sum/soma.js
+++
@@ -1,3 +0,0 @@
-export function somar(a, b) {
-	return a + b;
-}
